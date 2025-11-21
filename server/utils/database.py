1e746f170e964b80e873366f9aa90ce1df32a353
Helena Najwa <helenanajwa@ever-technologies.com>
2025-11-21 11:58:10 +0800

    Update database.py

--- server/utils/database.py
+++ server/utils/database.py
@@ -1,20 +1,23 @@
 import os
 from dotenv import load_dotenv
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+# Load environment variables
 load_dotenv()
 
-# Use psycopg (NOT psycopg2)
+# ⛔ DO NOT put your full URL inside getenv()
+# ✅ You must put only the VARIABLE NAME
 DATABASE_URL = os.getenv("postgresql+psycopg://shp_database_user:S9cC2qpSLoz09XyKH9hN2pOvcvdtlb8M@dpg-d4ft1lqdbo4c739qdqj0-a/shp_database
 ")
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is missing")
 
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Create the database connection manager
+engine = create_engine(DATABASE_URL)
+session_local = sessionmaker(autocommit=False, bind=engine)
 
 def get_db():
     db = session_local()
